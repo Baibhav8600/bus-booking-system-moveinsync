da911142a4d633c5c1f6b393d45c7f508bc03459
Baibhav <baibhav8600@gmail.com>
2024-01-22 15:51:07 +0530

    updated code

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,111 @@
+import React, { lazy, Suspense } from "react";
 import "./index.css";
+import "antd/dist/antd.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Loader from "./components/Loader";
+import PublicRoute from "./components/PublicRoute";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { useSelector } from "react-redux";
+
+const Index = lazy(() => import("./pages/Index"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const AdminBookings = lazy(() => import("./pages/Admin/AdminBookings"));
+const AdminBuses = lazy(() => import("./pages/Admin/AdminBuses"));
+const AdminUsers = lazy(() => import("./pages/Admin/AdminUsers"));
+const Home = lazy(() => import("./pages/Home"));
+const BookNow = lazy(() => import("./pages/BookNow"));
+const Bookings = lazy(() => import("./pages/Bookings"));
+
 function App() {
+  const { loading } = useSelector((state) => state.alerts);
   return (
-    <div className="App m-5 p-5">
-      <h1 className="text-2xl text-red-500">Easy-Booking</h1>
-      <button className="btn btn-primary">Click me</button>
+    <div className="App">
+      {loading && <Loader />}
+      <BrowserRouter>
+        <Suspense fallback={loading}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PublicRoute>
+                  <Index />
+                </PublicRoute>
+              }
+            />
+
+            <Route
+              path="/bus-booking"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/bookings"
+              element={
+                <ProtectedRoute>
+                  <Bookings />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/book-now/:id"
+              element={
+                <ProtectedRoute>
+                  <BookNow />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/admin/bookings"
+              element={
+                <ProtectedRoute>
+                  <AdminBookings />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/admin/buses"
+              element={
+                <ProtectedRoute>
+                  <AdminBuses />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/users"
+              element={
+                <ProtectedRoute>
+                  <AdminUsers />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/login"
+              element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <PublicRoute>
+                  <Register />
+                </PublicRoute>
+              }
+            />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
     </div>
   );
 }
